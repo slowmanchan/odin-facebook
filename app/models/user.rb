566b6b8a56c9f919750579131384067d9d42481c
Norman Chan <slowmanchan@gmail.com>
2016-07-09 16:47:08 -0400

    Added relationship functionality(following)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,26 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id"
+
+  has_many :following, through: :active_relationships, source: :followed
 
   def feed
     Post.where("user_id = ?", id)
+  end
+
+  # follows user
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # unfollows user
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
